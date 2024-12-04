7510befce0b9b2f00bcc0efba0ac89cc38c3128f
raquel686 <u201914478@upc.edu.pe>
2024-12-04 00:04:12 -0500

    dev: avance 2

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault(); // Evita que la página se recargue al enviar el formulario
-    alert('Formulario enviado correctamente');
-});
+
+
+// Lista de imágenes de fondo
+const backgrounds = [
+    'assets/fondo1.png',
+    'assets/fondo2.png',
+    'assets/fondo3.png'
+];
+
+let currentIndex = 0;
+
+// Función para cambiar el fondo
+function changeBackground() {
+   
+    const section = document.getElementById('inicio');
+    currentIndex = (currentIndex + 1) % backgrounds.length; // Incrementa y reinicia
+    section.style.background = `linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.7) 100%), url(${backgrounds[currentIndex]})`;
+    section.style.backgroundSize = "cover";
+    section.style.backgroundPosition = "center";
+    section.style.backgroundRepeat = "no-repeat"; 
+
+}
+
+// Ejecuta el cambio de fondo cada 2 segundos
+setInterval(changeBackground, 3000);
